877050afcd46cb144b3b16b5f4539928606dffe5
Torbjörn Gannholm <torbjorn.gannholm@tele2.com>
2018-12-08 07:58:35 +0100

    a7 after sacrificing two black goats with chicken heads

--- a7.sql
+++ a7.sql
@@ -6,19 +6,20 @@ create or replace table deps (
   post char(1) not null
 );
 
-load data local infile '/home/setorgan/aoc2018/a7test.txt'
+load data local infile '/home/setorgan/aoc2018/a7.txt'
 into table deps
 (@line)
 set pre = regexp_substr(@line, '^Step \\K.'),
     post = regexp_substr(@line, 'before step \\K.')
 ;
 
+-- severe black magic ordering by task everywhere to hope we get alphabetical order in @a
 create or replace table tasks as
 with t as (
   select pre task from deps
 union
   select post task from deps
-) select distinct(task) task from t;
+) select distinct(task) task from t order by task;
 
 create or replace table all_posts as
 select task, group_concat((case when post is not null then post else '' end) separator '') posts
@@ -29,22 +30,39 @@ group by task;
 with recursive chains as (
  select
    1 level,
-   (select group_concat(task separator '') from tasks) chain,
+   1 counter,
+   @l:=0 longest,
+   (select space(count(*)) from tasks) chain,
    (select group_concat(task separator '') from tasks) head,
-   (select group_concat(post separator '') from deps) tail
+   (select group_concat(post separator '') from deps) tail,
+   left(@a:=(select group_concat(task separator '') from tasks order by task),1) a
 union
  select distinct
    level + 1,
-   concat(regexp_replace(chain, task, ''), task) chain,
-   regexp_replace(head, task, '') head,
-   regexp_replace(tail, concat('(?U)([', posts, '])(?!.*\\1)'), '') tail
- from chains join all_posts
-      on head like concat('%', task, '%')
-      and (length(tail) = 0 or tail regexp concat ('(?U).*[', posts, '].*'))
-      and tail not like concat ('%', task, '%')
-) select chain from chains
-order by level desc, chain asc limit 1;
-
-
-
+   case when m is true then
+      1 else counter % length(@a) + 1 end,
+   @l:=(greatest(@l, length(trim(chain)))),
+   case when m is true then 
+     trim(concat(chain, task))
+     else chain end,
+   case when m is true then 
+     regexp_replace(head, task, '')
+     else head end,
+   case when m is true then 
+     regexp_replace(tail, concat('(?U)([', posts, '])(?!.*\\1)'), '')
+     else tail end,
+   case when m is true then
+     left(@a,1)
+     else substr(@a, counter % length(@a) + 1, 1) end
+ from 
+   (select level, counter, chain, head, tail, task, posts,
+      (task = a) m
+      from
+      chains join all_posts
+      where head like concat('%', task, '%')
+         and tail not like concat ('%', task, '%')
+-- not perfect, but at least some pruning
+         and length(trim(chain)) >= @l
+   ) t
+) select chain from chains order by level desc limit 1;
 
