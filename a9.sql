01ee1cf88887bb9e7b32abf238691cd0d5cae240
Torbjörn Gannholm <torbjorn.gannholm@tele2.com>
2018-12-09 16:08:45 +0100

    a9 part 1

--- a9.sql
+++ a9.sql
@@ -6,7 +6,9 @@ create or replace table indata (
   marbles int not null
 );
 
-insert into indata values (9, 25);
+-- insert into indata values (9, 25);
+-- insert into indata values (10, 1618);
+insert into indata values (424, 71144);
 
 create or replace table seed (
   players int not null,
@@ -37,11 +39,12 @@ union
   from a where i < players
 ) select players, marbles, circle, scores from a order by i desc limit 1;
 
+create or replace table result as
 with recursive game as (
   select
-    0 marble,
+    1 next_marble,
     marbles,
-    0 player,
+    1 next_player,
     players,
     circle,
     1 size,
@@ -50,15 +53,49 @@ with recursive game as (
   from init
 union
   select
-    marble + 1,
+    next_marble + 1,
     marbles,
-    (player % players) + 1,
+    (next_player + 1) % players,
     players,
-    json_array_insert(circle, concat('$[', (position + 1) % size + 1, ']'), marble + 1),
-    size + 1,
-    (position + 1) % size + 1,
-    scores
-  from game where marble < marbles
-) select * from game;
+    case when next_marble % 23 = 0 then
+      json_remove(circle, concat('$[', (position + size - 7) % size, ']'))
+    else
+      json_array_insert(circle, concat('$[', (position + 2) % size, ']'), next_marble)
+    end,
+    case when next_marble % 23 = 0 then
+      size - 1
+    else
+      size + 1
+    end,
+    case when next_marble % 23 = 0 then
+      (position + size - 7) % size
+    else
+      (position + 2) % size
+    end,
+    case when next_marble % 23 = 0 then
+      json_replace(scores, concat('$[', next_player, ']'),
+        json_value(scores, concat('$[', next_player, ']')) + next_marble +
+        json_value(circle, concat('$[', (position + size - 7) % size, ']')))
+    else
+      scores
+    end
+  from game where next_marble < marbles
+) select players, scores from game order by next_marble desc limit 1;
 
-    
+-- part 1
+with recursive s as (
+select
+  0 player,
+  0 score,
+  scores,
+  players
+from result
+union
+select
+  player + 1,
+  json_value(scores, concat('$[', (player + 1) % players, ']')),
+  scores,
+  players
+from s where player < players
+) select player, score from s order by score desc limit 1;
+
