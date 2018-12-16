2efdcac5e27dddaeb8ce9f0411a3d8cefca32214
Torbjörn Gannholm <torbjorn.gannholm@tele2.com>
2018-12-16 19:07:42 +0100

    Simplified and working

--- a15.dart
+++ a15.dart
@@ -18,64 +18,22 @@ class Grid {
 
 enum Direction {up, left, right, down}
 
-class Target {
-  final Being target;
-  final int distance;
-  final Direction direction;
-
-  Target(this.target, this.distance, this.direction);
-
-  @override
-  operator < (Target c) {
-    if (c == null) return true;
-    return distance < c.distance
-      || (distance == c.distance && direction.index < c.direction.index);}
-
-  Target extend(Direction to) {
-    return Target(target, distance + 1, to);
-  }
-
-  @override
-  String toString() {
-    return direction.toString() + distance.toString() + '->' + target.runtimeType.toString();
-  }
-}
-
 abstract class Square {
-  mark(Target t);
 
-  remove(Being b);
+  Being occupant;
 
-  add(Being b);
+  Map<Direction, Square> neighbours() => Map();
 
-  update(Direction to, Type beingType);
-
-  Target closest(Type enemy);
-
-  Map<Direction, Square> neighbours() {}
-
-  getUpdates(Type beingType) {}
-
-  List<Square> erasePath(Target fromTarget) { return List();}
+  @override
+  operator < (Square s) {
+    if (runtimeType != Cavern || s.runtimeType != Cavern) return null;
+    Cavern a = this as Cavern;
+    Cavern c = s as Cavern;
+    return a.row < c.row || (a.row == c.row && a.col < c.col);
+  }
 }
 
 class Rock extends Square {
-  @override
-  mark(Target t) {}
-
-  @override
-  remove(Being b) {}
-
-  @override
-  add(Being b) {
-    throw Exception();
-  }
-
-  @override
-  update(Direction to, Type beingType) {}
-
-  @override
-  Target closest(Type enemy) {}
 }
 
 class Cavern extends Square {
@@ -83,101 +41,7 @@ class Cavern extends Square {
   final int col;
   final Grid grid;
 
-  bool underUpdate = false;
-  Map<Type, Target> targets = Map();
-
   Cavern(this.row, this.col, this.grid);
-
-  @override
-  mark(Target t) {
-    Target current = targets[t.target.runtimeType];
-    if (t < current) {
-      targets[t.target.runtimeType] = t;
-      if (!isOccupied()) extendPaths(t);
-    }
-  }
-
-  void extendPaths(Target t) {
-    neighbours().forEach((Direction d, Square s) => s.mark(t.extend(reverse(d))));
-  }
-
-  @override
-  remove(Being b) {
-    Target current = targets[b.runtimeType];
-    if (current == null) {
-      return; // Already removed from other direction
-    }
-    if (current.target != b) {
-      return;
-    }
-    targets.remove(b.runtimeType);
-    var toFix = erasePathFromHere(current);
-    toFix.forEach((Square s) => s.getUpdates(b.runtimeType));
-    getUpdates(b.runtimeType);
-    targets.values.where((t) => t.target.runtimeType != b.runtimeType)
-      .forEach((t) => extendPaths(t));
-  }
-
-  @override
-  add(Being b) {
-    Map<Type, List<Square>> toUpdate = erasePaths();
-    targets.clear();
-    targets[b.runtimeType] = Target(b, 0, Direction.up);
-    neighbours().forEach((Direction d, Square s) => s.mark(Target(b, 1, reverse(d))));
-    toUpdate.forEach((Type type, List<Square> squares) {
-      squares.forEach((Square s) => s.getUpdates(type));
-      getUpdates(type);
-    });
-  }
-
-  Map<Type, List<Square>> erasePaths() {
-    Map<Type, List<Square>> toUpdate = Map();
-    targets.forEach((Type type, Target target) {
-      toUpdate[type] = erasePathFromHere(target);
-    });
-    return toUpdate;
-  }
-
-  List<Square> erasePathFromHere(Target target) {
-    return neighbours().values
-        .map((s) => s.erasePath(target)).expand((i) => i).toList();
-  }
-
-  List<Square> erasePath(Target fromTarget) {
-    Target target = targets[fromTarget.target.runtimeType];
-    if (target != null && fromTarget.target == target.target && target.distance == fromTarget.distance + 1) {
-      targets.remove(fromTarget.target.runtimeType);
-      var toUpdate = erasePathFromHere(target);
-      toUpdate.add(this);
-      return toUpdate;
-    }
-    return List();
-  }
-
-  @override
-  update(Direction to, Type beingType) {
-    Target current = targets[beingType];
-    if (current == null) {
-      getUpdates(beingType);
-      current = targets[beingType];
-    }
-    if (current != null && (!isOccupied() || current.distance == 0)) {
-      neighbours()[to].mark(current.extend(reverse(to)));
-    }
-  }
-
-  @override
-  getUpdates(Type beingType) {
-    if (underUpdate) return;
-    underUpdate = true;
-    neighbours().forEach((Direction d, Square s) => s.update(reverse(d), beingType));
-    underUpdate = false;
-  }
-
-  @override
-  Target closest(Type enemy) {
-    return targets[enemy];
-  }
 
   @override
   Map<Direction, Square> neighbours() {
@@ -189,26 +53,8 @@ class Cavern extends Square {
   }
 
   @override
-  operator < (Cavern c) {
-    return row < c.row || (row == c.row && col < c.col);
-  }
-
-  Direction reverse(Direction d) {
-    switch (d) {
-      case Direction.up: return Direction.down;
-      case Direction.down: return Direction.up;
-      case Direction.left: return Direction.right;
-      case Direction.right: return Direction.left;
-    }
-  }
-
-  @override
   String toString() {
-    return runtimeType.toString() + '(' + row.toString() + ', ' + col.toString() + '): {' + targets.toString() + '}';
-  }
-
-  bool isOccupied() {
-    return targets.values.any((t) => t.distance == 0);
+    return runtimeType.toString() + '(' + row.toString() + ', ' + col.toString() + '): {' + occupant.runtimeType.toString() + '}';
   }
 }
 
@@ -222,40 +68,70 @@ abstract class Being {
   Type enemy();
 
   bool takeTurn() {
-    Target closest = square.closest(enemy());
-    if (closest == null) {
-      return false; // Game is won
+    bool couldMove = true;
+    if (!inRange(square)) couldMove = move();
+    if (couldMove) strike();
+    return couldMove;
+  }
+
+  bool move() {
+    Set<Square> visited = Set.of([square]);
+    Map<Square, Direction> atDistance = square.neighbours()
+        .map((Direction d, Square s) => MapEntry(s, d));
+    atDistance.removeWhere((Square s, Direction d) => s is Rock || s.occupant != null);
+    while (atDistance.isNotEmpty) {
+      var possibleTargets = atDistance.keys
+          .where((s) => inRange(s));
+      var target = possibleTargets.isEmpty? null : possibleTargets
+          .reduce((a,b) => b < a ? b : a);
+      if (target != null) {
+        square.occupant = null;
+        square = square.neighbours()[atDistance[target]];
+        square.occupant = this;
+        return true;
+      }
+      visited.addAll(atDistance.keys);
+      Map<Square, Direction> nextDistance = Map();
+      atDistance.forEach((Square origin, Direction start) {
+        origin.neighbours().values.where((s) => !visited.contains(s) && !(s is Rock) && s.occupant == null)
+            .forEach((s) {
+          nextDistance.update(s, (d) => d.index < start.index ? d : start, ifAbsent: () => start);
+        });
+      });
+      atDistance = nextDistance;
     }
-    if (closest.distance > 1) {
-      square.remove(this);
-      square = square.neighbours()[closest.direction];
-      square.add(this);
-      closest = square.closest(enemy());
-    }
-    if (closest.distance == 1) {
-      square.neighbours().values.map((Square s) => s.closest(enemy()))
-          .where((Target t) => t != null && t.distance == 0)
-          .map((t) => t.target)
-          .reduce((a, b) => b.hitPoints < a.hitPoints ? b : a)
-      .hit(attackValue);
-    }
-    return true;
+    return false;
+  }
+
+  bool inRange(Square s) {
+    return s.neighbours().values.any(containsEnemy);
+  }
+
+  bool containsEnemy(Square s) => enemy() == s.occupant?.runtimeType;
+
+  void strike() {
+    var enemiesInRange = square.neighbours().values.where(containsEnemy)
+      .map((s) => s.occupant);
+    var enemyInRange = enemiesInRange.isEmpty ? null :
+        enemiesInRange.reduce((a, b) {
+          if (b.hitPoints < a.hitPoints) return b;
+          if (a.hitPoints < b.hitPoints) return a;
+          if (b.square < a.square) return b;
+          return a;
+        });
+    if (enemyInRange != null) enemyInRange.hit(attackValue);
   }
 
   void hit(int attackValue) {
     hitPoints -= attackValue;
     if (hitPoints <= 0) {
-      square.remove(this);
+      square.occupant = null;
     }
-  }
-
-  putOnMap() {
-    square.add(this);
   }
 
   @override
   String toString() {
-    return runtimeType.toString() + ': {' + square.toString() + '}\n';
+    return runtimeType.toString() + hitPoints.toString() + ': {' + square.toString() + '}\n';
   }
 }
 
@@ -304,7 +180,7 @@ void main() {
     }
     grid.addRow(row);
   }
-  beings.forEach((b) => b.putOnMap());
+  beings.forEach((b) => b.square.occupant = b);
   part1(beings);
 }
 
